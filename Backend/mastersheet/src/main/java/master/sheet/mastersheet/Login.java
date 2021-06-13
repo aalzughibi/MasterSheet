298913bbf95b3a73ccc2c1269bda3fe671f229ca
Abdullah Alzughibi <53004274+abdullah0102@users.noreply.github.com>
2021-06-13 14:30:58 +0300

    Login

--- Backend/mastersheet/src/main/java/master/sheet/mastersheet/Login.java
+++ Backend/mastersheet/src/main/java/master/sheet/mastersheet/Login.java
@@ -16,6 +16,9 @@ import io.jsonwebtoken.Jwts;
 // import org.apache.tomcat.util.http.MediaType;
 import org.springframework.http.*;
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 import org.springframework.web.bind.annotation.*;
 import io.jsonwebtoken.SignatureAlgorithm;
 import master.sheet.mastersheet.User.User;
@@ -23,7 +26,12 @@ import master.sheet.mastersheet.User.UserPassword;
 @RestController
 @RequestMapping("Login")
 public class Login {
-	public  String setJWT(String subject,String uid,String expired,String username){
+	public static String port = "3306";
+    public static String database = "mastersheetdatabase";
+    public static String username = "root";
+    public static String password = "1234";
+    public static String userTable="user";
+	public  String setJWT(String subject,String uid,Object expired,String username){
 		try {
 			String secretkey="NovaIsbestWaterBrand";
 		
@@ -53,9 +61,59 @@ String[] jwt = {header,payload};
 return jwt;
 // end decode jwt
 	}
+	public static User checkLogin(String userStr,String pass){
+		try{
+
+			Class.forName("org.mariadb.jdbc.Driver");  
+			Connection con=DriverManager.getConnection(  
+			"jdbc:mariadb://localhost:"+port+"/"+database+"?allowPublicKeyRetrieval=true&useSSL=false",username,password);
+			Statement stmt = con.createStatement();
+			String sql = "select count(*) from " +userTable + " where username='"+userStr+"' and password='"+pass+"'";
+            ResultSet rs=stmt.executeQuery(sql);  
+            rs.next();
+			User user;
+			System.out.println("outside");
+			if (rs.getInt(1)==1){
+				user =new User();
+				System.out.println(pass);
+				sql = "select * from " +userTable + " where username='"+userStr+"' and password='"+pass+"'";
+				rs=stmt.executeQuery(sql);
+				rs.next();
+				System.out.println(rs.getString(2));
+				user.setUsername(rs.getString(2));
+                user.setEmail(rs.getString(3));
+                user.setRole(rs.getInt(5));
+                user.setFirst_name(rs.getString(6));
+                user.setLast_name(rs.getString(7));
+                user.setDisplay_name(rs.getString(8));
+                user.setUid(rs.getString(9));
+                user.setBirthDate(rs.getString(10));
+                user.setFirst_time(rs.getInt(11));
+				return user;
+			}
+			return null;
+		}catch(Exception e){
+			System.out.println(e);
+			return null;
+		}
+	}
     @PostMapping
-    public ResponseEntity<HashMap<String,String>> LoginUser(){
-        System.out.println(getJWTToken(setJWT("Login", "moahmedkjns jsbjhs", "duhduiidu", "abody403"))[1]);
-return ResponseEntity.ok().build();
+    public ResponseEntity<HashMap<String,Object>> LoginUser(@RequestBody LoginUser lu){	
+		// check username and password
+		User user = checkLogin(lu.getUsername(),lu.getPassword());
+		if (user!=null){
+			// if auth return JWT
+			LocalDateTime ldt = LocalDateTime.now().plusHours(1);
+			Object expired = ldt.toEpochSecond(ZoneOffset.UTC);
+			String jwt = setJWT("LOGIN_AUTH", user.getUid(),expired , user.getUsername());
+			HashMap data =new HashMap();
+			data.put("JWT",jwt);
+			Boolean isFirstTime =user.getFirst_time()==1?false:true;
+			data.put("first_time",isFirstTime);
+			data.put("expired", expired);
+
+			return new ResponseEntity<>(data,HttpStatus.OK);
+		}
+		return ResponseEntity.noContent().build();
     }
 }
---
+++ Backend/mastersheet/src/main/java/master/sheet/mastersheet/LoginUser.java
@@ -0,0 +1,18 @@
+package master.sheet.mastersheet;
+
+public class LoginUser {
+    private String username;
+    private String password;
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
