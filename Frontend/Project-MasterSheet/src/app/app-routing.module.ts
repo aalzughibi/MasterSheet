1cd50de170641f2194a974cff236668ec90a81ac
shahadAlsayed <sh4322@hotmail.com>
2021-06-14 14:51:34 +0300

    login and register

--- Frontend/Project-MasterSheet/src/app/app-routing.module.ts
+++ Frontend/Project-MasterSheet/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistarComponent } from './registar/registar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registar', component: RegistarComponent }, 
+
+  { path: '**', redirectTo:'/login'  } // Wildcard Route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
