e1841538a3041adf1c806faf110ba30c3f07a598
Sulaiman Alkadi <suliman.qa@gmail.com>
2021-06-15 10:28:53 +0300

    ResetPass-UsersTable

--- Frontend/Project-MasterSheet/src/app/app.module.ts
+++ Frontend/Project-MasterSheet/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegistarComponent } from './registar/registar.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { UsersTableComponent } from './users-table/users-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistarComponent
+    RegistarComponent,
+    ResetPasswordComponent,
+    UsersTableComponent
   ],
   imports: [
     BrowserModule,
